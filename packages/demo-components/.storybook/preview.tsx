008aa813b95c8e2892e86400baa827d9320fa5bd
Leo Vigna <leo.vigna@gmail.com>
2022-05-08 18:46:37 +0200

    refactor demo-app

--- packages/demo-components/.storybook/preview.tsx
+++ packages/demo-components/.storybook/preview.tsx
@@ -1,29 +1,3 @@
-/*
-console.log('Buffer load')
-//https://github.com/vitejs/vite/discussions/2785
-import { Buffer as BufferPolyfill } from 'buffer'
-declare var Buffer: typeof BufferPolyfill;
-globalThis.Buffer = BufferPolyfill
-
-console.log('buffer', Buffer.from('foo', 'hex'))
-*/
-
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core'
-import { ThemeProvider } from 'styled-components';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Network, Contract, TestData, store } from '@owlprotocol/web3-redux';
-
-import { THEME_COLORS } from '../src/constants';
-import { WalletContext } from '../src/constants/web3React'
-import { getLibrary } from '../src/utils/getLibrary'
-import { withMockData } from '../src/hoc';
-
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../src/theme';
-
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -32,48 +6,13 @@ export const parameters = {
             date: /Date$/,
         },
     },
-    backgrounds: {
-        default: 'owl',
-        values: [
-            {
-                name: 'owl',
-                value: THEME_COLORS['theme1'].color5,
-            },
-            {
-                name: 'white',
-                value: THEME_COLORS['theme1'].color7,
-            },
-        ],
-    },
+    backgrounds: {},
 }
-
-//Browser wallet context provider
-const Web3ProviderWallet = createWeb3ReactRoot(WalletContext)
 
 export const decorators = [
     (Story) => {
-        const StoryWithData = withMockData(Story, [
-            Network.create({ networkId: '1' }),     //Mainnet
-            Network.create({ networkId: '42161' }), //Arbitrum
-            Network.create({ networkId: '10' }),    //Optimism
-            Network.create({ networkId: '137' }),   //Polygon
-            Contract.create(TestData.contractVITALIK),
-            Contract.create(TestData.contractWETH),
-        ]);
         return (
-            <ThemeProvider theme={THEME_COLORS.theme1}>
-                <Web3ReactProvider getLibrary={getLibrary}>
-                    <Web3ProviderWallet getLibrary={getLibrary}>
-                        <Router>
-                            <Provider store={store}>
-                                <ChakraProvider theme={theme}>
-                                    <StoryWithData />
-                                </ChakraProvider>
-                            </Provider>
-                        </Router>
-                    </Web3ProviderWallet>
-                </Web3ReactProvider>
-            </ThemeProvider>
+            <Story />
         )
     }
 ];
