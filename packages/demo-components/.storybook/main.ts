acd4490daa4d67c50a411df29e6988ebff421e62
Leo Vigna <leo.vigna@gmail.com>
2022-05-08 18:43:16 +0200

    refactor monorepo

---
+++ packages/demo-components/.storybook/main.ts
@@ -0,0 +1,140 @@
+//NodeJS Polyfills
+//https://medium.com/@ftaioli/using-node-js-builtin-modules-with-vite-6194737c2cd2
+const NodeGlobalsPolyfillPlugin = require('@esbuild-plugins/node-globals-polyfill').NodeGlobalsPolyfillPlugin
+const NodeModulesPolyfillPlugin = require('@esbuild-plugins/node-modules-polyfill').NodeModulesPolyfillPlugin
+const rollupNodePolyFill = require('rollup-plugin-node-polyfills')
+///require('rollup-plugin-polyfill-node');
+const rollupInject = require('@rollup/plugin-inject')
+const rollupCommonJS = require('@rollup/plugin-commonjs')
+const rollupAlias = require('@rollup/plugin-alias')
+
+//const dts = require('vite-dts').default
+const EnvironmentPlugin = require('vite-plugin-environment').default;
+
+//Typescript, ESLint check
+const Checker = require('vite-plugin-checker').default;
+//SVGR
+const svgrPlugin = require('vite-plugin-svgr');
+
+module.exports = {
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+    ],
+    framework: "react",
+    core: {
+        builder: "storybook-builder-vite"
+    },
+    features: {
+        storyStoreV7: true,
+        interactionsDebugger: true,
+    },
+    async viteFinal(config: any) {
+        console.debug(config)
+
+        config.optimizeDeps = config.optimizeDeps ?? {}
+        config.optimizeDeps.include = [
+            ...(config.optimizeDeps?.include ?? []),
+            '@owlprotocol/web3-redux',
+            '@storybook/testing-library',
+            '@storybook/jest',
+            'js-sha3',
+            'web3'
+        ];
+        config.optimizeDeps.exclude = [
+            ...(config.optimizeDeps?.exclude ?? []),
+        ]
+
+        // Enable esbuild polyfill plugins
+        config.optimizeDeps.esbuildOptions = config.optimizeDeps.esbuildOptions ?? {}
+
+        config.optimizeDeps.esbuildOptions.define = {
+            global: 'globalThis'
+        }
+
+        config.optimizeDeps.esbuildOptions.plugins = [
+            ...(config.optimizeDeps?.esbuildOptions?.plugins ?? []),
+            NodeGlobalsPolyfillPlugin({
+                process: true,
+                buffer: true,
+            }),
+            //NodeModulesPolyfillPlugin(),
+        ]
+
+        config.plugins = [
+            rollupInject({
+                Buffer: ['buffer', 'Buffer'],
+            }),
+            //Expose envars with traditional process.env
+            EnvironmentPlugin('all', { prefix: 'VITE_' }),
+            svgrPlugin({
+                svgrOptions: {
+                    icon: true,
+                },
+            }),
+            Checker({
+                typescript: true,
+                overlay: true,
+                eslint: {
+                    lintCommand: 'eslint --ext .ts,.tsx src --fix',
+                },
+            }),
+            ...(config.plugins ?? []),
+        ];
+
+        config.resolve.alias = {
+            ...(config.resolve?.alias ?? {}),
+            //process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+            stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+            http: 'rollup-plugin-node-polyfills/polyfills/http',
+            https: 'rollup-plugin-node-polyfills/polyfills/http',
+            buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+            //web3: 'web3/dist/web3.min.js'
+        }
+
+        config.define = {
+            ...(config.define ?? {}),
+        }
+
+        config.build = {
+            ...(config.build ?? {}),
+            commonjsOptions: {
+                transformMixedEsModules: true, //Include web3
+                /*
+                include: [
+                    '../../node_modules/.pnpm/web3-utils@1.7.0/node_modules/web3-utils/lib/utils.js'
+                ]
+                */
+                //exclude: [
+                //'../../node_modules/.pnpm/rlp@2.2.7/node_modules/rlp/dist.browser/index.js'
+                //]
+            },
+            rollupOptions: {
+                preserveModules: true,
+                plugins: [
+                    // Enable rollup polyfills plugin
+                    // used during production bundling
+                    /*
+                    rollupAlias({
+                        entries: [{
+                            find: 'buffer', replacement: 'rollup-plugin-node-polyfills/polyfills/buffer-es6'
+                        }]
+                    }),
+                    */
+                    /*
+
+                     */
+                ],
+            },
+        }
+
+        console.debug(config)
+
+        return config;
+    }
+};
---
+++ packages/demo-components/.storybook/preview.tsx
@@ -0,0 +1,79 @@
+/*
+console.log('Buffer load')
+//https://github.com/vitejs/vite/discussions/2785
+import { Buffer as BufferPolyfill } from 'buffer'
+declare var Buffer: typeof BufferPolyfill;
+globalThis.Buffer = BufferPolyfill
+
+console.log('buffer', Buffer.from('foo', 'hex'))
+*/
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core'
+import { ThemeProvider } from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Network, Contract, TestData, store } from '@owlprotocol/web3-redux';
+
+import { THEME_COLORS } from '../src/constants';
+import { WalletContext } from '../src/constants/web3React'
+import { getLibrary } from '../src/utils/getLibrary'
+import { withMockData } from '../src/hoc';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../src/theme';
+
+export const parameters = {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    backgrounds: {
+        default: 'owl',
+        values: [
+            {
+                name: 'owl',
+                value: THEME_COLORS['theme1'].color5,
+            },
+            {
+                name: 'white',
+                value: THEME_COLORS['theme1'].color7,
+            },
+        ],
+    },
+}
+
+//Browser wallet context provider
+const Web3ProviderWallet = createWeb3ReactRoot(WalletContext)
+
+export const decorators = [
+    (Story) => {
+        const StoryWithData = withMockData(Story, [
+            Network.create({ networkId: '1' }),     //Mainnet
+            Network.create({ networkId: '42161' }), //Arbitrum
+            Network.create({ networkId: '10' }),    //Optimism
+            Network.create({ networkId: '137' }),   //Polygon
+            Contract.create(TestData.contractVITALIK),
+            Contract.create(TestData.contractWETH),
+        ]);
+        return (
+            <ThemeProvider theme={THEME_COLORS.theme1}>
+                <Web3ReactProvider getLibrary={getLibrary}>
+                    <Web3ProviderWallet getLibrary={getLibrary}>
+                        <Router>
+                            <Provider store={store}>
+                                <ChakraProvider theme={theme}>
+                                    <StoryWithData />
+                                </ChakraProvider>
+                            </Provider>
+                        </Router>
+                    </Web3ProviderWallet>
+                </Web3ReactProvider>
+            </ThemeProvider>
+        )
+    }
+];
