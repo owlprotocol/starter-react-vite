1cc043ec7ed8fd8695254dc77bd233ca1f64689e
Leo Vigna <leo.vigna@gmail.com>
2022-05-08 20:21:04 +0200

    delete build

--- packages/demo-components/storybook-static/assets/preview.f48ace42.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./jsx-runtime.67f9f49e.js";import"./index.229e7bac.js";const e={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},backgrounds:{}},s=[o=>r(o,{})];export{s as decorators,e as parameters};
-//# sourceMappingURL=preview.f48ace42.js.map
