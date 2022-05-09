792d374b3bf16e28b72851cb804d4fc4f4337be7
Leo Vigna <leo.vigna@gmail.com>
2022-05-09 14:45:35 +0200

    fix eslint

--- packages/demo-app/.eslintrc.js
+++ packages/demo-app/.eslintrc.js
@@ -6,10 +6,11 @@ module.exports = {
     extends: [
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+        'plugin:prettier/recommended',
         'plugin:import/typescript',
         'plugin:storybook/recommended',
     ],
-    plugins: ['@typescript-eslint', 'prettier', 'import', 'react-hooks'],
+    plugins: ['@typescript-eslint', 'import', 'react-hooks'],
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
@@ -24,23 +25,15 @@ module.exports = {
         'no-console': 'off',
         'no-unused-vars': 'off',
         'no-empty': 'warn',
+        'import/no-named-as-default': 'off',
+        'import/no-anonymous-default-export': 'off',
+        'import/no-cycle': 'error',
+        'import/no-self-import': 'error',
         '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
-        'prettier/prettier': ['warn', prettierrc],
-        'import/no-named-as-default': 'off',
-        'import/no-anonymous-default-export': 'off',
-        'import/no-cycle': 'error',
-        'import/no-self-import': 'error',
-        //https://dev.to/diballesteros/how-to-quickly-configure-eslint-for-import-sorting-2h73
-        'import/order': [
-            1,
-            {
-                groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'],
-            },
-        ],
         'react/react-in-jsx-scope': 'off',
         'react/jsx-filename-extension': [
             1,
@@ -51,6 +44,13 @@ module.exports = {
         'react/prop-types': 0,
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
         'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+        //https://dev.to/diballesteros/how-to-quickly-configure-eslint-for-import-sorting-2h73
+        'import/order': [
+            1,
+            {
+                groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'],
+            },
+        ],
     },
     settings: {
         react: {
